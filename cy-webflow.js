b5c91f09c057642a78526c54bf1c0709180a1dce
chriswinsatlife <1500666+chriswinsatlife@users.noreply.github.com>
2020-01-14 01:12:20 -0400

    Update cy-webflow.js

--- cy-webflow.js
+++ cy-webflow.js
@@ -1,48 +1,50 @@
-  var products = function (str) {
-    if (!str) {
-        str = 'empty';
-    }
-    var names = str.split(',');
-    return str.replace(/[!\"#$%&'\(\)\*\+\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '')
-        .replace(/ /g, "-")
-        .replace(/,/g, " ")
-        .toLowerCase()
-        .trim()
-        .split(' ')
-        .map(function (slug, index) {
-            return "<a class='sidebar-tag-link blue big-tag' href='/products/" + slug + "'>" + names[index] + "</a> ";
-        })
-        ;
-};
-
-$(".product-array").each(function() {
-    var html = $(this).html();
-    $(this).html(products(html));
-});
 
 
- var papers = function (str) {
-    if (!str) {
-        str = 'empty';
-    }
-    var strings = str.split(',');
-    return str.replace(/[!\"#$%&'\(\)\*\+\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '')
-        .replace(/ /g, "-")
-        .replace(/,/g, " ")
-        .toLowerCase()
-        .trim()
-        .split(' ')
-        .map(function (x, index) {
-            return "<a class='help-category-card w-inline-block' href='/papers/" + x 
-                        + '"><div class='48'><img src='"
-                        + encodeURI('https://global-uploads.webflow.com/5df525c935688c0af50a9c57/5dfa8621f8d0d37b310dbb11_NoteIcon.png')
-                        + "'></div><div class='feature-info-right'><div class='comma-space serif small-text-2'>"
-                        + strings + "</div></div></a>";
-        })
-        ;
-};
+function getLinkFromTagText(tagText) {
+var slug = tagText.replace(/[!\"#$%&'\(\)\*\+\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '');
+return "<a class='sidebar-tag-link blue big-tag' href='/tag/" + slug + "'>" + tagText + "</a>";
+}
 
-$(".paper-array").each(function() {
-    var html = $(this).html();
-    $(this).html(papers(html));
-});
+function getLinkFromPaperText(paperText) {
+var slug = paperText.replace(/[!\"#$%&'\(\)\*\+\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '');
+return "<a class='sidebar-tag-link blue big-tag' href='/paper/" + slug + "'>" + paperText + "</a>";
+}
+
+function getLinkFromProductText(productText) {
+var slug = productText.replace(/[!\"#$%&'\(\)\*\+\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '');
+return "<a class='sidebar-tag-link blue big-tag' href='/product/" + slug + "'>" + productText + "</a>";
+}
+
+function getLinksHtmlFromTagsString(tagsString) {
+var linksList = tagsString.split(',').map(getLinkFromTagText);
+return linksList.join(' ');
+}
+
+function getLinksHtmlFromPapersString(papersString) {
+var linksList = papersString.split(',').map(getLinkFromPaperText);
+return linksList.join(' ');
+}
+
+function getLinksHtmlFromProductsString(productsString) {
+var linksList = productsString.split(',').map(getLinkFromProductText);
+return linksList.join(' ');
+}
+
+function addLinksToTagsStringElement() {
+    var tagsString = $(this).html();
+    $(this).html(getLinksHtmlFromTagsString(tagsString));
+}
+
+function addLinksToPapersStringElement() {
+    var papersString = $(this).html();
+    $(this).html(getLinksHtmlFromPapersString(papersString));
+}
+
+function addLinksToProductsStringElement() {
+    var productsString = $(this).html();
+    $(this).html(getLinksHtmlFromProductsString(productsString));
+}
+
+$(".tag-array").each(addLinksToTagsStringElement);
+$(".paper-array").each(addLinksToTagsStringElement);
+$(".product-array").each(addLinksToProductsStringElement);
