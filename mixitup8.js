a737075b32218b33d7b1cea68bb7c47ab53e804e
chriswinsatlife <1500666+chriswinsatlife@users.noreply.github.com>
2020-01-15 15:09:31 -0400

    Update and rename mixitup7.js to mixitup8.js

--- mixitup8.js
+++ mixitup8.js
@@ -8,6 +8,10 @@ var imported2 = document.createElement('script');
 imported2.src = 'https://cdnjs.cloudflare.com/ajax/libs/mixitup/3.3.1/mixitup.min.js';
 document.head.appendChild(imported2);
 
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var $ = jQuery = require('jquery');
+var mixitup = require('mixitup');
+
 
 	// 2) Reusable function to convert any string/text to css-friendly format
 	  var conv = function (str) {
