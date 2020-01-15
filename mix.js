aea3501dcac20d8aeaaa681c2b5a534d318ac685
chriswinsatlife <1500666+chriswinsatlife@users.noreply.github.com>
2020-01-15 13:01:41 -0400

    Update mix.js

--- mix.js
+++ mix.js
@@ -4,71 +4,73 @@ $.getScript('https://cdnjs.cloudflare.com/ajax/libs/mixitup/3.3.1/mixitup.min.js
 {
     // script is now loaded and executed.
     // put your dependent JS here.
-});
 
 
-// 2) Reusable function to convert any string/text to css-friendly format
-  var conv = function (str) {
-    if (!str) {
-        str = 'empty';
-    }  return str.replace(/[!\"#$%&'\(\)\*\+\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, ' ')
-				.replace(/, /g, ",")
-				.replace(/ /g, "-")
-			    .replace(/,/g, " ")
-			    .toLowerCase()
-    			.trim();
-};
+	// 2) Reusable function to convert any string/text to css-friendly format
+	  var conv = function (str) {
+	    if (!str) {
+		str = 'empty';
+	    }  return str.replace(/[!\"#$%&'\(\)\*\+\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, ' ')
+					.replace(/, /g, ",")
+					.replace(/ /g, "-")
+				    .replace(/,/g, " ")
+				    .toLowerCase()
+				.trim();
+	};
 
-// 3) Creating dynamic elements classes from its categories for filtering:
+	// 3) Creating dynamic elements classes from its categories for filtering:
 
-	var catArray = document.querySelectorAll('.filter-category');
-    catArray.forEach( function(elem) {
-        var text1 = elem.innerText || elem.innerContent;
-        var text2 = conv(text1);
-            text2.split(' ').forEach(function(className) { 
-            elem.parentElement.parentElement.parentElement.parentElement.classList.add(className);
-            });
-        });
+		var catArray = document.querySelectorAll('.filter-category');
+	    catArray.forEach( function(elem) {
+		var text1 = elem.innerText || elem.innerContent;
+		var text2 = conv(text1);
+		    text2.split(' ').forEach(function(className) { 
+		    elem.parentElement.parentElement.parentElement.parentElement.classList.add(className);
+		    });
+		});
 
-// Some Jetboost shit which can likely be deleted
+	// Some Jetboost shit which can likely be deleted
 
-//  var catArray = document.querySelectorAll('.jetboost-list-item-hide');
-//    catArray.forEach( function(elem) {
-//    elem.parentElement.classList.add('jetboost-list-item-hide');
-//  });
-  
+	//  var catArray = document.querySelectorAll('.jetboost-list-item-hide');
+	//    catArray.forEach( function(elem) {
+	//    elem.parentElement.classList.add('jetboost-list-item-hide');
+	//  });
 
-// 4) Creating custom data-date attributes from blog dates:
-//  var sortArray = document.querySelectorAll('.w-dyn-item .sort-category');
-//  sortArray.forEach( function(sortElem) {
-//    var sortText = sortElem.innerText || sortElem.innerContent;
-//    sortElem.parentElement.parentElement.parentElement.setAttribute('data-date', sortText);
-//  });
 
-// 4.5) to add filtering on paginated items
- const moveItems = document.querySelectorAll('.to-be-moved');
-    moveItems.forEach( function(move) {
-        document.getElementById('target-container').appendChild(move);
-    });
+	// 4) Creating custom data-date attributes from blog dates:
+	//  var sortArray = document.querySelectorAll('.w-dyn-item .sort-category');
+	//  sortArray.forEach( function(sortElem) {
+	//    var sortText = sortElem.innerText || sortElem.innerContent;
+	//    sortElem.parentElement.parentElement.parentElement.setAttribute('data-date', sortText);
+	//  });
 
-// 5) Set the reference to the container. Use the class-name of your Collection List or ID of the Collection List
+	// 4.5) to add filtering on paginated items
+	 const moveItems = document.querySelectorAll('.to-be-moved');
+	    moveItems.forEach( function(move) {
+		document.getElementById('target-container').appendChild(move);
+	    });
 
-  var containerEl = document.querySelector('.mixitup');
+	// 5) Set the reference to the container. Use the class-name of your Collection List or ID of the Collection List
 
-// As targets are filtered in and out, they will fade between
-// opacity 1 and 0 and transform between translateZ(-100px) and
-// translateZ(0).
+	  var containerEl = document.querySelector('.mixitup');
 
-var mixer = mixitup(containerEl, {
-    animation: {
-    		enable: false,
-        effects: 'fade',
-	      nudge: false,
-        reverseOut: false,
-				applyPerspective: false,
-				animateResizeContainer: false,
-        clampHeight: false,
-        clampWidth: false,
-        duration: 200
-    }
+	// As targets are filtered in and out, they will fade between
+	// opacity 1 and 0 and transform between translateZ(-100px) and
+	// translateZ(0).
+
+	var mixer = mixitup(containerEl, {
+	    animation: {
+			enable: false,
+		effects: 'fade',
+		      nudge: false,
+		reverseOut: false,
+					applyPerspective: false,
+					animateResizeContainer: false,
+		clampHeight: false,
+		clampWidth: false,
+		duration: 200
+	    }
+	});
+
+
 });
