ab1a89e313c6d45e2b75ef8031af5e3163bbddc7
chriswinsatlife <1500666+chriswinsatlife@users.noreply.github.com>
2020-01-15 14:09:49 -0400

    Update mixitup.js

--- mixitup.js
+++ mixitup.js
@@ -1,6 +1,24 @@
 // 1) Call Mixitup
 
-$.getScript('https://cdnjs.cloudflare.com/ajax/libs/mixitup/3.3.1/mixitup.min.js', function()
+const getScript = (source, callback) => {
+    var script = document.createElement('script');
+    var prior = document.getElementsByTagName('script')[0];
+    script.async = 1;
+
+    script.onload = script.onreadystatechange = (_, isAbort) => {
+        if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
+            script.onload = script.onreadystatechange = null;
+            script = undefined;
+            if (!isAbort) if (callback) callback();
+        }
+    };
+
+    script.src = source;
+    prior.parentNode.insertBefore(script, prior);
+};
+
+$document.getScript('https://cdnjs.cloudflare.com/ajax/libs/mixitup/3.3.1/mixitup.min.js')
+	    
 {
     // script is now loaded and executed.
     // put your dependent JS here.
