f4158f3e7a850c429b3d50bd4a2f8f2f49a9b977
chriswinsatlife <1500666+chriswinsatlife@users.noreply.github.com>
2020-01-15 14:19:28 -0400

    Update mixitup1.js

--- mixitup1.js
+++ mixitup1.js
@@ -1,22 +1,5 @@
 // 1) Call Mixitup
 
-const getScript = (source, callback) => {
-    var script = document.createElement('script');
-    var prior = document.getElementsByTagName('script')[0];
-    script.async = 1;
-
-    script.onload = script.onreadystatechange = (_, isAbort) => {
-        if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
-            script.onload = script.onreadystatechange = null;
-            script = undefined;
-            if (!isAbort) if (callback) callback();
-        }
-    };
-
-    script.src = source;
-    prior.parentNode.insertBefore(script, prior);
-};
-
 $.getScript('https://cdnjs.cloudflare.com/ajax/libs/mixitup/3.3.1/mixitup.min.js', function()
 	    
 {
