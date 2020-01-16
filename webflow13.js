340a0a0e2196e65a6b00a9d7f5bd49fa6958561a
chriswinsatlife <1500666+chriswinsatlife@users.noreply.github.com>
2020-01-16 15:09:51 -0400

    Update and rename webflow12.js to webflow13.js

--- webflow13.js
+++ webflow13.js
@@ -29,7 +29,7 @@ function getLinkFromTacticText(tacticText) {
 	var slug = tacticText.replace(/[!\"#$%&'\(\)\*\+\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '')
 			.replace(/ /g, "-")
 			.toLowerCase();
-	return "<div class='w-dyn-item w-col w-col-3'><a href='/tactics/"+slug+"' class='help-category-card vertical w-inline-block'><img src='https://global-uploads.webflow.com/5df525c935688c0af50a9c57/5e16948ccb9d510239dd0ed8_tbi-mark-dark%403x.png' width='42' alt='' class='image-7'><h5 class='heading-17'>"+tacticText+"</h5><div class='text-small-2 splitter w-condition-invisible'><span style='display: none;'>Products Go Here</span></div><div class='text-small-2 splitter'><span style='display: none;'>Papers Go Here</span></div></a></div>"
+	return "<div class='w-dyn-item w-col w-col-3'><a href='/tactics/"+slug+"' class='help-category-card vertical w-inline-block'><img src='https://global-uploads.webflow.com/5df525c935688c0af50a9c57/5e16948ccb9d510239dd0ed8_tbi-mark-dark%403x.png' width='42' alt='' class='image-7'><h5 class='heading-17'>"+tacticText+"</h5></a></div></div>"
 	;
 }
 
@@ -37,7 +37,7 @@ function getLinkFromBehaviorText(behaviorText) {
 	var slug = behaviorText.replace(/[!\"#$%&'\(\)\*\+\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '')
 			.replace(/ /g, "-")
 			.toLowerCase();
-	return "<div class='collection-item-6 w-dyn-item w-col w-col-4'><a href='/behaviors/"+slug+"' class='help-category-card product-card tactic-card w-inline-block'><div class='feature-icon-left _48'><img src='https://global-uploads.webflow.com/5df525c935688c0af50a9c57/5e16948ccb9d510239dd0ed8_tbi-mark-dark%403x.png' alt='' class='image-8'></div><div class='feature-info-right'><div class='comma-space product-name tactic-name'>"+behaviorText+"</div></div></a></div>"
+	return "<div class='collection-item-6 w-dyn-item w-col w-col-4'><a href='/behaviors/"+slug+"' class='help-category-card product-card tactic-card w-inline-block'><div class='feature-icon-left _48'><img src='https://global-uploads.webflow.com/5df525c935688c0af50a9c57/5e16948ccb9d510239dd0ed8_tbi-mark-dark%403x.png' alt='' class='image-8'></div><div class='feature-info-right'><div class='comma-space product-name tactic-name'>"+behaviorText+"</div></a></div>"
 	;
 }
 
